582aa5905802802dc058a87a9e98e474c828d90d
Shelvacu <github@shelvacu.com>
2021-03-29 22:55:02 -0700

    Add highscores

--- main.js
+++ main.js
@@ -23,7 +23,22 @@ let count = 0;
 let fail = false;
 let failEl = document.getElementById("fail");
 let speeds = document.getElementById("speeds");
+let lvlind = document.getElementById("level");
+let chrind = document.getElementById("characters");
+let hs_speeds = document.getElementById("hs-speeds");
+let hs_lvlind = document.getElementById("hs-level");
+let hs_chrind = document.getElementById("hs-characters");
+let farthest = +localStorage.getItem('farthest') || 0;
+let levelSpeeds = [];
 const aCode = "A".charCodeAt(0);
+
+let currLevel = 2;
+while(true) {
+  let score = localStorage.getItem("levelSpeed" + currLevel);
+  if (!score) break;
+  levelSpeeds.push(score+0);
+  currLevel++;
+}
 
 function trailingZeros(n) {
   let res = 0;
@@ -35,10 +50,9 @@ function trailingZeros(n) {
   return res;
 }
 
-let lvlind = document.getElementById("level");
-let chrind = document.getElementById("characters");
 
 function updateCounts(thisCharAt){
+  if (inp.disabled) return;
   chrind.textContent = count+"";
   let lvlFloat = Math.log2(count+1);
   lvlind.textContent = lvlFloat.toFixed(5);
@@ -50,14 +64,43 @@ function updateCounts(thisCharAt){
   let speedtext = speed_c_per_s.toFixed(3) + " characters per second (" + (count-1) + " characters* in " + time_s + " seconds)";
   console.log(lvlFloat);
   console.log(lvlFloat|0);
+  if( count > farthest ){
+    farthest = count;
+    localStorage.setItem("farthest", farthest);
+    updateHs();
+  }
   if( (lvlFloat|0) === lvlFloat ){
     let el = speeds.children[speeds.children.length-1];
     el.textContent = "Level " + lvlFloat + " speed: " + speedtext;
     speeds.appendChild(document.createElement("div"));
+
+    if (levelSpeeds[lvlFloat-2] == null || time_s < levelSpeeds[lvlFloat-2]) {
+      levelSpeeds[lvlFloat-2] = time_s;
+      localStorage.setItem("levelSpeed" + lvlFloat, time_s);
+    }
+    updateHs();
   }
   let el = speeds.children[speeds.children.length-1];
   el.textContent = "Current Speed: " + speedtext;
 }
+
+function updateHs(){
+  let lvlFloat = Math.log2(farthest+1);
+  hs_lvlind.textContent = lvlFloat.toFixed(5);
+  hs_chrind.textContent = farthest+"";
+  let level = 2;
+  hs_speeds.innerHTML = "";
+  for(let time_s of levelSpeeds){
+    let count = Math.pow(2,level)-1;
+    let speed_c_per_s = count / time_s;
+    let d = document.createElement("div");
+    d.textContent = `Level ${level} best speed: ${speed_c_per_s.toFixed(3)} characters per second (${count-1} characters in ${time_s} seconds)`
+    hs_speeds.appendChild(d);
+    level++;
+  }
+}
+
+updateHs();
 
 function doFail(why){
   inp.disabled = true;
